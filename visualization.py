c18320be65949ac5cb90c74deec884b605ed18c3
huawenbo <hwb0856@outlook.com>
2025-03-25 10:27:27 +0800

    update visualization.py

--- visualization.py
+++ visualization.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+if __name__ == '__main__':
+    # Load the data
+    data = pd.read_csv('data.csv')
+
+    # Plot the data
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x='x', y='y', data=data)
+    plt.title('Data')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
